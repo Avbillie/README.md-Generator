b0b2b77472214c6001ea014c7586f0c98ad6c0a9
Anthony_Billie_Jr <67110957+Avbillie@users.noreply.github.com>
2020-10-11 13:09:13 -0400

    updated program prompts along with its design

---
+++ Develop/README.md
@@ -0,0 +1,41 @@
+
+[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)]   
+# README.md generator
+
+## Description
+
+This program takes user input from a series of prompts and generates a README.md file for thier repository. 
+
+## Table of Contents
+
+[1.](#Description)
+[2.](#Installation)
+[3.](#Usage)
+[4.](#License)
+[5.](#Contributing)
+[6.](#Tests)
+[7.](#Questions)
+
+## Installation
+
+Before you  run the program make sure to install the inquirer package or you will get a throw error. \n ``` npm i inquirer ``` 
+
+## Usage
+
+You will be presented with a series of prompts that  will input your responses into the designated sections of the readme.md file.
+
+## License
+
+[Perl](https://opensource.org/licenses/Artistic-2.0)
+
+## Contributing
+
+Feel free to contribute to this repository, I only ask to keep your work as professional as possible and be respectful of others code.
+
+## Tests
+
+## Questions
+
+[GitHub](https://github.com/avbillie)
+[Email](anthonybilliejr2021@gmail.com)
+            
---
+++ Develop/index.js
@@ -0,0 +1,187 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+createReadMe();
+
+async function createReadMe() {
+  await inquirer
+    .prompt([
+      {
+        message:
+          "Welcome to README generator we will use your repository information to begin and finish the process, press enter to continue...",
+        name: "continue"
+      },
+      {
+        type: "input",
+        message: "Enter your project title...",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "Please give your project a description...",
+        name: "description"
+      },
+      {
+        type: "input",
+        message: "Please provide the project installation instructions...",
+        name: "instructions"
+      },
+      {
+        type: "input",
+        message: "Please provide the project usage information...",
+        name: "usageInfo"
+      },
+      {
+        type: "input",
+        message: "Please provide user contribution guidelines...",
+        name: "contributeGuidelines"
+      },
+      {
+        type: "list",
+        message:
+          "Select a license you want to apply from the following options:",
+        choices: ["Mozilla", "Perl", "IBM"],
+        name: "licenseSelection"
+      },
+      {
+        type: "input",
+        message: "Enter your gitHub username...",
+        name: "gitHubUsername"
+      },
+      {
+        type: "input",
+        message: "Enter your email address...",
+        name: "email"
+      }
+    ])
+    .then(function (data) {
+
+      const { title } = data;
+      const { description } = data;
+      const { instructions } = data;
+      const { usageInfo } = data;
+      const { contributeGuidelines } = data;
+      const { licenseSelection } = data;
+      const { gitHubUsername } = data;
+      const { email } = data;
+      const gitHubUrl = `[GitHub](https://github.com/${gitHubUsername})`;
+      const emailAddress = `[Email](${email})`;
+      let license = "";
+      let licenseInfo = "";
+      const responseArray = [
+        {
+          input: title,
+          a: "Project title succesfully added to README.md!",
+          b: "Project title not added README.md."
+        },
+        {
+          input: description,
+          a: "Project description succesfully added to README.md!",
+          b: "Project description not added README.md."
+        },
+        {
+          input: instructions,
+          a: "Project instructions succesfully added to README.md!",
+          b: "Project instructions not added README.md."
+        },
+        {
+          input: usageInfo,
+          a: "Project usage information succesfully added to README.md!",
+          b: "Project usage information not added README.md."
+        },
+        {
+          input: contributeGuidelines,
+          a: "Project contribution guidelines succesfully added to README.md!",
+          b: "Project contribution guidelines not added README.md."
+        },
+        {
+          input: licenseSelection,
+          a: "Project license succesfully added to README.md!",
+          b: "Project license not added README.md."
+        },
+        {
+          input: gitHubUsername,
+          a: "GitHub link succesfully added to README.md!",
+          b: "GitHub link not added README.md."
+        },
+        {
+          input: email,
+          a: "Email link succesfully added to README.md!",
+          b: "Email link not added to README.md."
+        }
+      ];
+
+      for (i = 0; i < responseArray.length; i++) {
+        if (responseArray[i].input != " ") {
+          console.log(responseArray[i].a);
+        } else {
+          console.log(responseArray[i].b);
+        }
+      }
+
+      if (licenseSelection == "Mozilla") {
+        license =
+          "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]";
+        licenseInfo = "[Mozilla](https://opensource.org/licenses/MPL-2.0)";
+      } else if (licenseSelection == "Perl") {
+        license =
+          "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)]";
+        licenseInfo = "[Perl](https://opensource.org/licenses/Artistic-2.0)";
+      } else if (licenseSelection == "IBM") {
+        license =
+          "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)]";
+        licenseInfo = "[IBM](https://opensource.org/licenses/IPL-1.0)";
+      }
+
+      const readMe = `
+${license}   
+# ${title}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+[1.](#Description)
+[2.](#Installation)
+[3.](#Usage)
+[4.](#License)
+[5.](#Contributing)
+[6.](#Tests)
+[7.](#Questions)
+
+## Installation
+
+${instructions}
+
+## Usage
+
+${usageInfo}
+
+## License
+
+${licenseInfo}
+
+## Contributing
+
+${contributeGuidelines}
+
+## Tests
+
+## Questions
+
+${gitHubUrl}
+${emailAddress}
+            
+`;
+      fs.writeFile("README.md", readMe, function (err) {
+        if (err) {
+          console.log("README.md not created.");
+          throw err;
+        } else {
+          console.log("README.md created.");
+        }
+      });
+    });
+}
